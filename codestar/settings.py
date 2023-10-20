c250c6a9469259cb211f5b2224e182f04fbf9d75
BanditCook <tanguy.lalexandre@gmail.com>
2023-10-20 09:29:51 +0100

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-tanguy.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog-tanguy-c97c2aee030f.herokuapp.com', 'localhost']
 
 
 # Application definition
